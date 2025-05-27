5bc972c8429ceb19013cbf29ada2f7691a422b60
Koushik-Zzz <Koushik02269@gmail.com>
2025-05-27 14:32:41 +0530

    Finally added shadcn

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -1,26 +1,10 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import { Button } from "@repo/ui/components/ui/button";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default function Home() {
   return (
-    <div className="bg-red-500">
+    <div className="">
+      <Button>Finally</Button>
       <div className="bg-red-500"></div>
       Hi there
     </div>
